b56f17df7f0fd59a571ca743349a4b0f8bbd4399
Vladislav Khapin <hapinvlad@gmail.com>
2018-07-08 23:47:53 +0300

    Started work on benchmarks

---
+++ src/EmitMapper.Benchmarks/MapperBenchmark.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Security.Cryptography;
+using AutoFixture;
+using AutoMapper;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Exporters;
+using BenchmarkDotNet.Attributes.Jobs;
+using EmitMapper.MappingConfiguration;
+using EmitMapper.MappingConfiguration.MappingOperations;
+
+namespace EmitMapper.Benchmarks
+{
+    public class AuthorModel
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+    }
+
+    public class AuthorDTO
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+    }
+
+    [ MarkdownExporter]
+    public class MapperBenchmark
+    {
+        [Benchmark]
+        public void EmitMapper()
+        {
+            var mapper = ObjectMapperManager.DefaultInstance.GetMapper<AuthorModel, AuthorDTO>();
+            Fixture fixture = new Fixture();
+
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                var source = fixture.Create<AuthorModel>();
+                var result = mapper.Map(source);
+                var temp = result.Address;
+            }
+        }
+
+        [Benchmark]
+        public void AutoMapper()
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<AuthorModel, AuthorDTO>();
+            });
+            Fixture fixture = new Fixture();
+            var mapper = config.CreateMapper();
+
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                var source = fixture.Create<AuthorModel>();
+                var result = mapper.Map<AuthorModel, AuthorDTO>(source);
+                var temp = result.Address;
+            }
+        }
+    }
+}
---
+++ src/EmitMapper.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace EmitMapper.Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<MapperBenchmark>();
+        }
+    }
+}
